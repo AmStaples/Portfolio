f7cc5d339c00150615d1744996746d80e5f2cac9
AmStaples <150167944+AmStaples@users.noreply.github.com>
2024-01-21 20:46:33 -0600

    Added test page and javascript file.

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+/*
+Author: Aaron Staples
+Date:1/21/2024
+Description: Portfolio javascript page for learning.
+*/
+
+let firstint = 1;
+firstint += 1;
+let uname = prompt("Enter your name");
+console.log("The firstint variable is set to " + firstint);
+console.log("The name variable is set to " + uname);

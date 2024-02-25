a93cce4ccf1b5d4dbc4e6bba30fc483cf68e2468
AmStaples <150167944+AmStaples@users.noreply.github.com>
2024-02-25 13:59:57 -0600

    Objects and classes assignment

---
+++ Unit2/lib_portfolio.js
@@ -0,0 +1,13 @@
+
+class FirstClass{
+    constructor(field1, field2) {
+        this.slot1 = field1;
+        this.slot2 = field2;
+
+        
+    }
+    respond() {
+        console.log(this.slot1);
+        console.log(this.slot2);
+    }
+}
---
+++ Unit2/objects.js
@@ -0,0 +1,7 @@
+
+
+const obj1 = new FirstClass(10, 11);
+const obj2 = new FirstClass(12, 13);
+
+obj1.respond();
+obj2.respond();
